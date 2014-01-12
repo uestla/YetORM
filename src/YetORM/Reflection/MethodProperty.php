6c170d356cd71eeb9766770604f08b4632f4113c
Petr Kessler <kesspess@gmail.com>
2014-01-12 01:14:58 +0100

    new year

--- src/YetORM/Reflection/MethodProperty.php
+++ src/YetORM/Reflection/MethodProperty.php
@@ -4,7 +4,7 @@
 /**
  * This file is part of the YetORM library
  *
- * Copyright (c) 2013 Petr Kessler (http://kesspess.1991.cz)
+ * Copyright (c) 2013, 2014 Petr Kessler (http://kesspess.1991.cz)
  *
  * @license  MIT
  * @link     https://github.com/uestla/YetORM
