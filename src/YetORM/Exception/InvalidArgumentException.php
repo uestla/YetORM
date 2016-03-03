07439cb832809d208e55119955f679d168317821
uestla <kesspess@gmail.com>
2016-03-03 13:17:20 +0100

    updated phpDocs

--- src/YetORM/Exception/InvalidArgumentException.php
+++ src/YetORM/Exception/InvalidArgumentException.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the YetORM library
  *
- * Copyright (c) 2013, 2015 Petr Kessler (http://kesspess.1991.cz)
+ * Copyright (c) 2013, 2016 Petr Kessler (http://kesspess.1991.cz)
  *
  * @license  MIT
  * @link     https://github.com/uestla/YetORM
