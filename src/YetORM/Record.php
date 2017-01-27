2d515ad91ce2696f538eb8b725cb9c5b487fc5e9
Petr Kessler <kesspess@gmail.com>
2017-01-27 01:33:38 +0100

    updated phpDocs

--- src/YetORM/Record.php
+++ src/YetORM/Record.php
@@ -1,11 +1,10 @@
 <?php
 
 /**
- * This file is part of the YetORM library
- *
- * Copyright (c) 2013, 2016 Petr Kessler (https://kesspess.cz)
+ * This file is part of the YetORM package
  *
  * @license  MIT
+ * @author   Petr Kessler (https://kesspess.cz)
  * @link     https://github.com/uestla/YetORM
  */
 
